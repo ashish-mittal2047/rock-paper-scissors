b2165913decc55b89ad752b796deda11ddceea3f
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-10 17:38:15 +0530

    Add playGame function Decide number of rounds for the game, get human and computer choice for each round of the game, call playRound, and finally print the message at the end of the game.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function playGame() {
+  let numOfRounds = 5;
+  for (let i = 0; i < numOfRounds; i++) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+  if (humanScore < computerScore) {
+    console.log(`Congratulations! You are the overall winner.`);
+  }
+  else if (humanScore === computerScore) {
+    console.log(`Overall, the game has tied!`);
+  }
+  else console.log(`You lose! Better luck next time.`);
+}
